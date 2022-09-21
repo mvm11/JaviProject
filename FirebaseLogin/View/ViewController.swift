d98459354603844103e67759b20626b426391b1b
Mateo Valencia Minota <mateo.valenciam@sofka.com.co>
2022-09-21 01:29:07 -0500

    Se realizan avances en el proyecto

---
+++ FirebaseLogin/View/ViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by Mateo Valencia on 20/09/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+    
+    //MARK: - @IBOutlets
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var user = User(email: "", password: "")
+    var activityIndicator:UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDelegates()
+        setActivityIndicator()
+        startButton.isEnabled = false
+    }
+    
+    func setDelegates()->Void {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    func setActivityIndicator()->Void{
+        activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+        activityIndicator.center = view.center
+        activityIndicator.isHidden = true
+        self.view.addSubview(activityIndicator)
+    }
+    
+    
+    func displayActivityIndicatorView() -> () {
+        self.view.isUserInteractionEnabled = false
+        self.view.bringSubviewToFront(self.activityIndicator)
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+      }
+
+       func hideActivityIndicatorView() -> () {
+         if !self.activityIndicator.isHidden{
+            DispatchQueue.main.async {
+                self.view.isUserInteractionEnabled = true
+                self.activityIndicator.stopAnimating()
+                self.activityIndicator.isHidden = true
+
+            }
+        }
+
+      }
+    
+    
+    
+    fileprivate func showErrorMessage(_ errorMessage : String) {
+        let alertController = UIAlertController(title: "UPS!", message: errorMessage, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Aceptar", style: .default))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func startButtonAction(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().signIn(withEmail: email, password: password){(result, error) in
+                self.displayActivityIndicatorView()
+                switch error {
+                    case .some(let error as NSError) where error.code == AuthErrorCode.wrongPassword.rawValue:
+                    self.displayActivityIndicatorView()
+                    self.showErrorMessage("Contraseña incorrecta")
+                    self.hideActivityIndicatorView()
+                case .some(let error as NSError) where error.code == AuthErrorCode.userNotFound.rawValue:
+                    self.displayActivityIndicatorView()
+                    self.showErrorMessage("Correo incorrecto")
+                    self.hideActivityIndicatorView()
+                    case .some(let error):
+                    self.displayActivityIndicatorView()
+                    self.showErrorMessage("Login error: \(error.localizedDescription)")
+                    self.hideActivityIndicatorView()
+                    case .none:
+                    if (result?.user) != nil {
+                        self.hideActivityIndicatorView()
+                        print("Entrooooo")
+                      }
+                }
+            }
+        }
+    }
+    
+
+    fileprivate func validateFields() -> Bool {
+            return (passwordTextField.text!.count >= 8) && (isValidEmail(emailTextField.text ?? ""))
+        }
+        
+
+        fileprivate func updateView() {
+            if(validateFields()){
+                startButton.isEnabled = true
+                startButton.tintColor = UIColor.blue
+            }else{
+                startButton.isEnabled = false
+                startButton.tintColor = UIColor.gray
+            }
+        }
+
+}
+
+
+func isValidEmail(_ email: String) -> Bool {
+    let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+    let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    return emailPred.evaluate(with: email)
+}
+
+//MARK: - Text Field Delegate Methods
+
+extension ViewController : UITextFieldDelegate{
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString
+            string: String) -> Bool {
+            if textField == self.emailTextField {
+                updateView()
+            } else if textField == self.passwordTextField {
+                updateView()
+            }
+            return true
+        }
+    
+}
+
---
+++ FirebaseLogin/ViewModel/FirebaseViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseViewModel.swift
+//  FirebaseLogin
+//
+//  Created by Mateo Valencia on 20/09/22.
+//
+
+import Foundation
+import FirebaseAuth
